b57ea371b62ee278de06cd743633e503f39e7ded
Aymen-Mohammednur <aymenmohammednur12@gmail.com>
2021-05-20 18:18:58 +0300

    Lab6 linkedStructures finished

--- Lab6/linkedstructures.c
+++ Lab6/linkedstructures.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Books
 {
-    char *title;
-    char *author;
+    char title[20];
+    char author[20];
     int pages;
     struct Books *next;
 } Book;
@@ -37,18 +38,19 @@ void displayBooks()
     }
 
     while(temp1 != NULL) {
-      printf("Title: %s\nAuthor: %s\nPages: %d", temp1->title, temp1->author, temp1->pages);
+      printf("\nTitle: %s\nAuthor: %s\nPages: %d\n", temp1->title, temp1->author, temp1->pages);
       temp1 = temp1->next;
     }
 }
 
-struct Books* searchBook()
+void searchBook()
 {   
     struct Books *temp1 = start;
 
     if (temp1 == NULL) { 
         printf("There are no books in the library! \n");
     }
+
     else {
         char key[20];
         printf("Enter the book's title: ");
@@ -56,24 +58,22 @@ struct Books* searchBook()
 
         struct Books* current = start;
 
-        if(start == NULL) {
-            return NULL;
-        }
-
-        while (current->title != key) {
-            if (current->next == NULL) {
-                return NULL;
-            }
-            else {
+        while (current != NULL && strcmp(current->title, key) != 0) {
                 current = current->next;
-            }
         }
 
-        return current;
+        if (current == NULL) {
+            printf("No results found.\n");
+            return;
+        }
+        else {
+            printf("\nSearch Results...\n");
+            printf("\nTitle: %s\nAuthor: %s\nPages: %d\n", current->title, current->author, current->pages);
+        } 
     }
 }
 
-struct Books* deleteBook()
+void deleteBook()
 {   
     struct Books *temp1 = start;
 
@@ -89,38 +89,32 @@ struct Books* deleteBook()
         struct Books* current = start;
         struct Books* previous = NULL;
 
-        if(start == NULL) {
-            return NULL;
+        while (current != NULL && strcmp(current->title, key) != 0) {
+            previous = current;
+            current = current->next;
         }
 
-        while (current->title != key) {
-            if (current->next == NULL) {
-                return NULL;
-            }
-            else {
-                previous = current;
-                current = current->next;
-            }
+        if (current == NULL) {
+            printf("No such book to delete\n");
+            return;
         }
-
-        if (current == start) {
+        else if (current == start) {
             start = start->next;
         }
         else {
             previous->next = current->next;
         }
-
+        
         printf("\nBook Deleted!\n");
     }
 }
 
 void main() 
 {
-    printf("<<<<<<<<< WELCOME TO MY LIBRARY >>>>>>>>>>\n");
-
     int status = 1, choice;
     while (status == 1)
     {
+        printf("\n<<<<<<<<< WELCOME TO MY LIBRARY >>>>>>>>>>\n");
         printf("1 - to add a new book\n");
         printf("2 - to display the books in the library\n");
         printf("3 - to search a book\n");
